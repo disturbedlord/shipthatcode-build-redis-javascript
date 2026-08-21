4df1186761828723b7243cd5c657c5c43cb88840
Avinash K <todisturbedlord@gmail.com>
2026-08-21 20:39:22 +0530

    done

--- main.js
+++ main.js
@@ -2,38 +2,62 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const readline = require("readline");
 const rl = readline.createInterface({
-  input: process.stdin,
+    input: process.stdin,
 });
-const ParseQuotes = (x) => {
-  let text = "";
-  let i = 0;
-  while (i < x.length) {
-    if (x[i] !== '"') text += x[i];
-    i++;
-  }
-  return text;
-};
-var commandTable = new Map();
-commandTable.set("PING", (p) => {
-  if (p.length > 0) {
-    p = ParseQuotes(p);
-    return `$${p.length}\r\n${p}\r\n`;
-  } else return "+PONG\r\n";
-});
-const ParseCommand = (inp) => {
-  const cmd = inp.split(" ");
-  if (cmd && cmd[0])
-    if (commandTable.has(cmd[0])) {
-      const callback = commandTable.get(cmd[0]);
-      if (callback) {
-        return callback(cmd.slice(1).join(" ") || "");
-      }
-    }
-};
+class Redis {
+    execute = (command, args) => {
+        const callback = this[command];
+        if (typeof callback === "function") {
+            return callback(args);
+        }
+        else {
+            return "Command not found";
+        }
+    };
+    Normalize = (inp) => {
+        // Normalize the Inp, remove Quotes
+        let normalizedText = [];
+        let i = 0;
+        let stack = new Array();
+        let currText = "";
+        while (i < inp.length) {
+            if (inp[i] === '"' || inp[i] === "\'") {
+                if (stack.length > 0 && stack[-1] === inp[i]) {
+                    // Closing Tag
+                    stack.pop();
+                    normalizedText.push(currText);
+                    currText = "";
+                }
+                else
+                    stack.push(inp[i]);
+            }
+            else {
+                currText += inp[i];
+            }
+            i++;
+        }
+        if (currText.length > 0)
+            normalizedText.push(currText);
+        return normalizedText.join(" ");
+    };
+    PING = (args) => {
+        if (args) {
+            const norArg = this.Normalize(args);
+            return `$${norArg.length}\r\n${norArg}\r\n`;
+        }
+        else
+            return `+PONG\r\n`;
+    };
+}
+const redis = new Redis();
 rl.on("line", (line) => {
-  if (!line) return;
-  const response = ParseCommand(line);
-  process.stdout.write(response);
+    if (!line)
+        return;
+    const data = line.split(" ");
+    const cmd = data[0];
+    const args = data.slice(1).join(" ");
+    const response = redis.execute(cmd, args);
+    process.stdout.write(response);
 });
 rl.on("close", () => process.exit());
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -3,39 +3,56 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 
-const ParseQuotes = (x: string) => {
-  let text = "";
-  let i = 0;
-  while (i < x.length) {
-    if (x[i] !== '"') text += x[i];
-    i++;
-  }
-
-  return text;
-};
-
-var commandTable = new Map<string, (p: any) => any>();
-commandTable.set("PING", (p: string) => {
-  if (p.length > 0) {
-    p = ParseQuotes(p);
-    return `$${p.length}\r\n${p}\r\n`;
-  } else return "+PONG\r\n";
-});
-
-const ParseCommand = (inp: string) => {
-  const cmd = inp.split(" ");
-  if (cmd && cmd[0])
-    if (commandTable.has(cmd[0])) {
-      const callback = commandTable.get(cmd[0]);
-      if (callback) {
-        return callback(cmd.slice(1).join(" ") ?? "");
-      }
+class Redis {
+  execute = (command: string, args: string) => {
+    const callback = (this as any)[command];
+    if (typeof callback === "function") {
+      return callback(args);
+    } else {
+      return "Command not found";
     }
-};
+  };
 
+  private Normalize = (inp: string) => {
+    // Normalize the Inp, remove Quotes
+    let normalizedText = [] as string[];
+    let i = 0;
+    let stack = new Array();
+    let currText = "";
+    while (i < inp.length) {
+      if (inp[i] === '"' || inp[i] === "\'") {
+        if (stack.length > 0 && stack[-1] === inp[i]) {
+          // Closing Tag
+          stack.pop();
+          normalizedText.push(currText);
+          currText = "";
+        } else stack.push(inp[i]);
+      } else {
+        currText += inp[i];
+      }
+      i++;
+    }
+
+    if (currText.length > 0) normalizedText.push(currText);
+
+    return normalizedText.join(" ");
+  };
+
+  private PING = (args?: string) => {
+    if (args) {
+      const norArg: string = this.Normalize(args);
+      return `$${norArg.length}\r\n${norArg}\r\n`;
+    } else return `+PONG\r\n`;
+  };
+}
+
+const redis = new Redis();
 rl.on("line", (line: string) => {
   if (!line) return;
-  const response = ParseCommand(line);
+  const data = line.split(" ");
+  const cmd = data[0];
+  const args = data.slice(1).join(" ");
+  const response = redis.execute(cmd!, args);
   process.stdout.write(response);
 });
 
