8f84ad41f16f4c2334d8ca1163392f8bb9de2931
Avinash K <todisturbedlord@gmail.com>
2026-08-21 20:03:02 +0530

    done

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hi");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+});
+var commandTable = new Map();
+commandTable.set("PING", (p) => {
+    if (p.length > 0) {
+        return `$${p.length}\r\n${p}\r\n`;
+    }
+    else
+        return "+PONG\r\n";
+});
+const ParseCommand = (inp) => {
+    const cmd = inp.split(" ");
+    if (cmd && cmd[0])
+        if (commandTable.has(cmd[0])) {
+            const callback = commandTable.get(cmd[0]);
+            if (callback) {
+                return callback(cmd[1] ?? "");
+            }
+        }
+};
+rl.on("line", (line) => {
+    if (!line)
+        return;
+    const response = ParseCommand(line);
+    process.stdout.write(response);
+});
+rl.on("close", () => process.exit());
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-console.log("hi");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+var commandTable = new Map<string, (p: any) => any>();
+commandTable.set("PING", (p: string) => {
+  if (p.length > 0) {
+    return `$${p.length}\r\n${p}\r\n`;
+  } else return "+PONG\r\n";
+});
+
+const ParseCommand = (inp: string) => {
+  const cmd = inp.split(" ");
+  if (cmd && cmd[0])
+    if (commandTable.has(cmd[0])) {
+      const callback = commandTable.get(cmd[0]);
+      if (callback) {
+        return callback(cmd[1] ?? "");
+      }
+    }
+};
+
+rl.on("line", (line: string) => {
+  if (!line) return;
+  const response = ParseCommand(line);
+  process.stdout.write(response);
+});
+
+rl.on("close", () => process.exit());
