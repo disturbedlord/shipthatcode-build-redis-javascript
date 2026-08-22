5dbd19f42465461ca4c759e45e035e62e215e5d3
Avinash K <todisturbedlord@gmail.com>
2026-08-22 16:04:29 +0530

    done

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ const rl = readline.createInterface({
 });
 class Redis {
     constructor() {
+        this.inMemoryDS = new Map();
         this.ARITY = {
             PING: [0, 1],
             ECHO: [1, 1],
             COMMAND: [0, 0],
+            SET: [2, 2],
+            GET: [1, 1],
         };
         this.Serialization = {
             bulkString: (a) => {
@@ -42,6 +45,8 @@ class Redis {
             let i = 0;
             let stack = new Array();
             let currText = "";
+            if (!inp)
+                return currText;
             while (i < inp.length) {
                 if (inp[i] === '"' || inp[i] === "\'") {
                     if (stack.length > 0 && stack[stack.length - 1] === inp[i]) {
@@ -63,34 +68,97 @@ class Redis {
             return normalizedText.join(" ");
         };
         this.PING = (args) => {
-            if (args && args.length === 1) {
-                return this.Serialization.bulkString(args);
+            if (args.length === 1) {
+                return this.Serialization.bulkString(args[0]);
             }
             else
                 return `+PONG\r\n`;
         };
         this.ECHO = (args) => {
-            if (!args)
-                return this.Serialization.bulkString("");
-            else {
-                return this.Serialization.bulkString(args);
-            }
+            return this.Serialization.bulkString(args[0]);
         };
         this.COMMAND = (args) => {
             return this.Serialization.simpleString("OK");
         };
+        this.SET = (args) => {
+            const key = args[0];
+            const value = args[1];
+            this.inMemoryDS.set(key, value);
+            return this.Serialization.simpleString("OK");
+        };
+        this.GET = (args) => {
+            const key = args[0];
+            return this.Serialization.bulkString(this.inMemoryDS.get(key));
+        };
     }
 }
 const redis = new Redis();
+let argsLen = 0, argSize = 0, arg = "";
+let params = [];
+let state = 0; // 0 => size , 1 => args len , 2 => arg itself
+const parseCommand = (stream) => {
+    var _a, _b;
+    const read = (s, skip, len) => {
+        let i = 0;
+        let text = "";
+        if (!len)
+            len = s.length;
+        while (i < s.length && i < len) {
+            if (s[i] !== skip)
+                text += s[i];
+            i++;
+        }
+        return text;
+    };
+    if (!stream)
+        return;
+    switch (state) {
+        case 0: {
+            if (!stream.startsWith("*"))
+                return;
+            argsLen = (_a = parseInt(read(stream, "*"))) !== null && _a !== void 0 ? _a : 0;
+            //console.log(argsLen);
+            state = 1; // change to arg len parsing state
+            break;
+        }
+        case 1: {
+            if (!argsLen || !stream.startsWith("$"))
+                return;
+            argSize = (_b = parseInt(read(stream, "$"))) !== null && _b !== void 0 ? _b : 0;
+            //console.log(argSize);
+            state = 2; // change to arg parsing state
+            break;
+        }
+        case 2: {
+            if (!argSize)
+                return;
+            arg = read(stream, "", argSize);
+            //console.log(arg);
+            argsLen--;
+            params.push(arg);
+            if (argsLen > 0)
+                state = 1;
+            else
+                return false;
+        }
+    }
+    return true;
+};
 rl.on("line", (line) => {
     var _a;
     if (!line)
         return;
-    const data = line.split(" ");
-    const cmd = (_a = data[0]) === null || _a === void 0 ? void 0 : _a.toUpperCase();
-    const args = data.slice(1);
+    const keepReading = parseCommand(line);
+    if (keepReading)
+        return;
+    //console.log(params);
+    const cmd = (_a = params[0]) === null || _a === void 0 ? void 0 : _a.toUpperCase();
+    const args = params.slice(1);
     const response = redis.execute(cmd, args);
     process.stdout.write(response);
+    // Reset parser State to 0 to read length
+    state = 0;
+    params = [];
 });
 rl.on("close", () => process.exit());
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -4,14 +4,17 @@ const rl = readline.createInterface({
 });
 
 class Redis {
+  inMemoryDS = new Map<string, string>();
   ARITY = {
     PING: [0, 1],
     ECHO: [1, 1],
     COMMAND: [0, 0],
+    SET: [2, 2],
+    GET: [1, 1],
   };
 
   Serialization = {
-    bulkString: (a: string) => {
+    bulkString: (a?: string) => {
       const normalizedInp = this.Normalize(a);
 
       if (!normalizedInp) return `$-1\r\n`;
@@ -36,12 +39,13 @@ class Redis {
     }
   };
 
-  private Normalize = (inp: string) => {
+  private Normalize = (inp?: string) => {
     // Normalize the Inp, remove Quotes
     let normalizedText = [] as string[];
     let i = 0;
     let stack = new Array();
     let currText = "";
+    if (!inp) return currText;
     while (i < inp.length) {
       if (inp[i] === '"' || inp[i] === "\'") {
         if (stack.length > 0 && stack[stack.length - 1] === inp[i]) {
@@ -60,32 +64,97 @@ class Redis {
     return normalizedText.join(" ");
   };
 
-  private PING = (args?: string) => {
-    if (args && args.length === 1) {
-      return this.Serialization.bulkString(args);
+  private PING = (args: string[]) => {
+    if (args.length === 1) {
+      return this.Serialization.bulkString(args[0]!);
     } else return `+PONG\r\n`;
   };
 
-  private ECHO = (args?: string) => {
-    if (!args) return this.Serialization.bulkString("");
-    else {
-      return this.Serialization.bulkString(args);
-    }
+  private ECHO = (args: string[]) => {
+    return this.Serialization.bulkString(args[0]!);
   };
 
-  private COMMAND = (args?: string) => {
+  private COMMAND = (args: string[]) => {
     return this.Serialization.simpleString("OK");
+  };
+
+  private SET = (args: string[]) => {
+    const key = args[0]!;
+    const value = args[1]!;
+    this.inMemoryDS.set(key, value);
+    return this.Serialization.simpleString("OK");
+  };
+
+  private GET = (args: string[]) => {
+    const key = args[0]!;
+    return this.Serialization.bulkString(this.inMemoryDS.get(key));
   };
 }
 
 const redis = new Redis();
+let argsLen = 0,
+  argSize = 0,
+  arg = "";
+
+let params = [] as string[];
+
+let state = 0; // 0 => size , 1 => args len , 2 => arg itself
+const parseCommand = (stream: string) => {
+  const read = (s: string, skip: string, len?: number): string => {
+    let i = 0;
+    let text = "";
+    if (!len) len = s.length;
+
+    while (i < s.length && i < len) {
+      if (s[i] !== skip) text += s[i];
+      i++;
+    }
+    return text;
+  };
+
+  if (!stream) return;
+  switch (state) {
+    case 0: {
+      if (!stream.startsWith("*")) return;
+      argsLen = parseInt(read(stream, "*")) ?? 0;
+      //console.log(argsLen);
+      state = 1; // change to arg len parsing state
+      break;
+    }
+    case 1: {
+      if (!argsLen || !stream.startsWith("$")) return;
+      argSize = parseInt(read(stream, "$")) ?? 0;
+      //console.log(argSize);
+
+      state = 2; // change to arg parsing state
+      break;
+    }
+    case 2: {
+      if (!argSize) return;
+      arg = read(stream, "", argSize);
+      //console.log(arg);
+      argsLen--;
+      params.push(arg);
+      if (argsLen > 0) state = 1;
+      else return false;
+    }
+  }
+
+  return true;
+};
+
 rl.on("line", (line: string) => {
   if (!line) return;
-  const data = line.split(" ");
-  const cmd = data[0]?.toUpperCase();
-  const args = data.slice(1);
+  const keepReading = parseCommand(line);
+  if (keepReading) return;
+  //console.log(params);
+  const cmd = params[0]?.toUpperCase();
+  const args = params.slice(1);
   const response = redis.execute(cmd!, args);
   process.stdout.write(response);
+  // Reset parser State to 0 to read length
+  state = 0;
+  params = [] as string[];
 });
 
 rl.on("close", () => process.exit());
